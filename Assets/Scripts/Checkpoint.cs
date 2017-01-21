186e14ed06f1ecf5ba681cc9b8969a311d64ed7b
Max72590 <max-725@hotmail.com>
2017-01-21 13:36:10 +0100

    Checkpoint update

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+	public GameObject player;
+	private Player playerScript;
+	// Use this for initialization
+	void Start () {
+		playerScript = player.GetComponent<Player>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(transform.position.x) == Mathf.Abs(player.transform.position.x)){
+			// playerScript.SetRespawn(transform.position);
+		}
+	}
+
+
+}
