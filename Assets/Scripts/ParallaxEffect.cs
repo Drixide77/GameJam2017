0798e03f8e08c34fd469d8d3bd80b6139467ce43
Max72590 <max-725@hotmail.com>
2017-01-21 22:55:17 +0100

    Tiling & Parallax not finished

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour {
+
+	public Transform[] backgrounds;
+	private float[] parallaxScales;
+
+	public float smoothing;
+
+	private Transform camRef;
+	private Vector3 camLastPos;
+	// Use this for initialization
+	void Start () {
+		camRef = Camera.main.transform;
+		smoothing = 1.0f;
+		camLastPos = camRef.position;
+		parallaxScales = new float[backgrounds.Length];
+		for (int i = 0; i < backgrounds.Length; ++i) {
+			parallaxScales [i] = backgrounds [i].position.z*-1.0f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < backgrounds.Length; ++i) {
+			float parallax = (camLastPos.x - camRef.position.x) * parallaxScales [i];
+			float backgroundTargetX = backgrounds[i].position.x + parallax;
+			Vector3 newBackgroundPos = new Vector3 (backgroundTargetX, backgrounds[i].position.y, backgrounds[i].position.z);
+			backgrounds[i].position = Vector3.Lerp (backgrounds[i].position, newBackgroundPos, smoothing*Time.deltaTime);
+		}
+		camLastPos = camRef.position;
+	}
+}
---
+++ Assets/Scripts/Tiling.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class Tiling : MonoBehaviour {
+
+	public int offset;
+
+	bool hasLeftAuxiliar;
+	bool hasRightAuxiliar;
+
+	public float spriteWidth;
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = Camera.main;
+		offset = 2;
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		spriteWidth = sr.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasLeftAuxiliar || !hasRightAuxiliar) {
+			float camExtension = cam.orthographicSize * (Screen.width / Screen.height);
+			float LeftVisibleZone = transform.position.x + spriteWidth / 2.0f - camExtension;
+			float RightVisibleZone = transform.position.x - spriteWidth / 2.0f - camExtension;
+			if (transform.position.x >= RightVisibleZone && !hasRightAuxiliar) {
+			
+			}
+			if (transform.position.x <= LeftVisibleZone && !hasRightAuxiliar) {
+
+			}
+
+		}
+	}
+
+	void InstantiateNewSprite(int LeftOrRight){
+		Vector3 newPosition;
+	}
+}
