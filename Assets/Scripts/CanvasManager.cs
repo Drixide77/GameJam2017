4eac9b1f066cdf27f6a8250f9033af7e8227f4cb
Max72590 <max-725@hotmail.com>
2017-01-21 15:23:47 +0100

    Checkpoints and Main Menu added

---
+++ Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour {
+
+
+	public Button playButton;
+	public Button exitButton;
+	public Button selectedButton;
+
+	bool firstIsSelected;
+	// Use this for initialization
+	void Start () {
+		selectedButton = playButton;
+		firstIsSelected = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Vertical"))
+	}
+}
