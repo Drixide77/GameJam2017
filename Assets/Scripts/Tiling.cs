a04f118578e65e7563091e1fe631c07d44ff75dc
Max72590 <max-725@hotmail.com>
2017-01-22 09:21:06 +0100

    tiling script finished

--- Assets/Scripts/Tiling.cs
+++ Assets/Scripts/Tiling.cs
@@ -11,12 +11,15 @@ public class Tiling : MonoBehaviour {
 	bool hasLeftAuxiliar;
 	bool hasRightAuxiliar;
 
+	public bool reverseScale;
+
 	public float spriteWidth;
 	public Camera cam;
 
 	// Use this for initialization
 	void Start () {
 		cam = Camera.main;
+		reverseScale = false;
 		offset = 2;
 		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
 		spriteWidth = sr.sprite.bounds.size.x;
@@ -26,19 +29,31 @@ public class Tiling : MonoBehaviour {
 	void Update () {
 		if (!hasLeftAuxiliar || !hasRightAuxiliar) {
 			float camExtension = cam.orthographicSize * (Screen.width / Screen.height);
-			float LeftVisibleZone = transform.position.x + spriteWidth / 2.0f - camExtension;
-			float RightVisibleZone = transform.position.x - spriteWidth / 2.0f - camExtension;
-			if (transform.position.x >= RightVisibleZone && !hasRightAuxiliar) {
-			
+			float LeftVisibleZone = transform.position.x - spriteWidth / 2.0f - camExtension;
+			float RightVisibleZone = transform.position.x + spriteWidth / 2.0f - camExtension;
+			if (transform.position.x >= RightVisibleZone - offset && !hasRightAuxiliar) {
+				InstantiateNewSprite (1);
+				hasRightAuxiliar = true;
 			}
-			if (transform.position.x <= LeftVisibleZone && !hasRightAuxiliar) {
-
+			if (transform.position.x <= LeftVisibleZone + offset && !hasLeftAuxiliar) {
+				InstantiateNewSprite (-1);
+				hasLeftAuxiliar = true;
 			}
 
 		}
 	}
 
 	void InstantiateNewSprite(int LeftOrRight){
-		Vector3 newPosition;
+		Vector3 newPosition= new Vector3(transform.position.x + spriteWidth * LeftOrRight,transform.position.y,transform.position.z);
+		Transform newSprite = (Transform)Instantiate (transform, newPosition,Quaternion.identity);
+		if (reverseScale) {
+			newSprite.localScale = new Vector3 (newSprite.localScale.x*-1,newSprite.localScale.y,newSprite.localScale.z);
+		}
+		newSprite.parent = transform;
+		if (LeftOrRight < 0) {
+			newSprite.GetComponent<Tiling> ().hasLeftAuxiliar = true;
+		} else {
+			newSprite.GetComponent<Tiling> ().hasRightAuxiliar = true;		
+		}
 	}
 }
